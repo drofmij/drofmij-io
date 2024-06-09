cf3573e8edb93b21b5458fbb3b3643e4a2bc78ab
Jim Ford <drofmij@gmail.com>
2024-06-09 13:25:17 -0400

    update source and target version and added doc blocks to Nibbler

--- src/main/java/io/github/drofmij/io/Nibbler.java
+++ src/main/java/io/github/drofmij/io/Nibbler.java
@@ -16,31 +16,68 @@ import java.io.InputStreamReader;
  */
 public abstract class Nibbler implements Closeable {
 
+    /**
+     * name of the file to nibble
+     */
     private String filename;
 
+    /**
+     * input stream to read from
+     */
     private InputStream inputstream;
 
+    /**
+     * default constructor
+     */
     public Nibbler() {
     }
 
+    /**
+     * constructor, sets file name to read from
+     *
+     * @param filename the string of the file name we will be reading from
+     */
     public Nibbler(String filename) {
         this.filename = filename;
     }
 
+    /**
+     * constructor, sets inputstream to read from
+     *
+     * @param inputstream input stream object
+     */
     public Nibbler(InputStream inputstream) {
         this.inputstream = inputstream;
     }
-  
+
+    /**
+     * convenience method for setting input stream
+     *
+     * @param inputstream the input stream object to read
+     * @return this Nibbler object is returned.
+     */
     public Nibbler withInputStream(InputStream inputstream) {
         this.inputstream = inputstream;
         return this;
     }
 
+    /**
+     * convenience method for setting filename
+     *
+     * @param filename the string of the file name we will be reading from
+     * @return this Nibbler object is returned.
+     */
     public Nibbler withFilename(String filename) {
         this.filename = filename;
         return this;
     }
 
+    /**
+     * read file line by line, if filename is not null, create FileInputStream to read the file, if inputstream is not
+     * null, read directly from it
+     *
+     * @throws IOException throws IO exception if we run into an issue
+     */
     public void nibble() throws IOException {
         if (filename != null) {
             File file = new File(filename);
@@ -54,9 +91,16 @@ public abstract class Nibbler implements Closeable {
         }
     }
 
+    /**
+     * read the given InputStreamReader via a BufferedReader, each line will be passed to handleLine() as implemented in
+     * the desecending class.
+     *
+     * @param inputStream the input stream object to read
+     * @throws IOException throws IO exception if we run into an issue
+     */
     private void nibble(InputStream inputStream) throws IOException {
         try (InputStreamReader isr = new InputStreamReader(inputStream);
-                BufferedReader br = new BufferedReader(isr);) {
+             BufferedReader br = new BufferedReader(isr);) {
             String line;
             while ((line = br.readLine()) != null) {
                 handleLine(line);
@@ -64,12 +108,23 @@ public abstract class Nibbler implements Closeable {
         }
     }
 
+    /**
+     * override close(), closes the input stream
+     *
+     * @throws IOException throws IO exception if we run into an issue
+     */
     @Override
     public void close() throws IOException {
-        if(inputstream != null) {
+        if (inputstream != null) {
             inputstream.close();
         }
     }
 
+    /**
+     * descending class will implement this method and handle each line as appropriate, can be anonymously implemented
+     * if appropriate.
+     *
+     * @param line string value of the line to handle.
+     */
     public abstract void handleLine(String line);
 }
